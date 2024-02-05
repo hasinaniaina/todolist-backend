d6361855b14b15238f1fd0cb053723315dce5e48
android <harilantorakotondrainibe@gmail.com>
2024-02-05 14:49:46 +0300

    Origin of request change to

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true, 
-    origin: 'http://localhost:5173',
+    origin: 'https://todolist-73iy.onrender.com',
     methods: ['GET', 'POST']
 }));
 
